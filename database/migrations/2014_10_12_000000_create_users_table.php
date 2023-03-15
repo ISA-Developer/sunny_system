d50112902bf7e788c343a77efdbdedb5b6b53e7b
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2023-03-15 17:07:51 +0700

    Update 2014_10_12_000000_create_users_table.php
    penyesuain nama dan tipe kolom

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid');
-            $table->string('name');
+            $table->string('id', 36);
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('is_active')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->primary('id');
         });
     }
 
