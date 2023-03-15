13b960b38b5899c9b2b0ee2b96874e95684acb3d
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2023-03-15 17:14:11 +0700

    Install Laravel Breeze
    - Install Laravel Breeze
    - Penambahan logic, route login/logout untuk json dan sanctum
    - login bisa menggunakan username / email

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthenticatedSessionController::class, 'storeSanctum'])
+    ->name('login.sanctum');
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/logout', [AuthenticatedSessionController::class, 'destroySanctum'])
+        ->name('logout.sanctum');
+
+
 });
